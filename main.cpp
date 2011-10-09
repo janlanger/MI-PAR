6b04320c2ba830b60755c0fb555d2f0201d05a61
Quinix <quinix@quinix.cz>
2011-10-09 18:51:29 +0200

    Algorithmic solution (works for max 4 discs, 3 poles, grater values takes more than 2G of memory)

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <time.h>
 
 #include "Item.h"
 #include "Disc.h"
@@ -20,7 +21,7 @@ Item* generateInitState(int n, int s){
     Item* item = new Item(s, n);
     for( int i = 0 ; i<n; i++){
         Disc* disc = new Disc(n-i);
-        Pole* pole = item->getPole(/*(time(NULL) % s))*/0);
+        Pole* pole = item->getPole(/*(time(NULL) % s)*/0);
         pole->addDisc(disc);
     }
     item->generateOptions();
@@ -28,49 +29,64 @@ Item* generateInitState(int n, int s){
     return item;
 }
 
+int getUpperBound(int n, int s) {
+	//(2n/(s-2)-1)(2s-5)
+	return (int) (pow((double)2,(n/(s-2)))-1)*(2*s-5);
+}
+
+
 /*
  * 
  */
 int main(int argc, char** argv) {
 
-    int n = 3;
+    int n = 5;
     int s = 3;
     int f = 2;
     
-    int limit = 100;
-    int counter = 0;  
+    int limit = getUpperBound (n,s);
+	cout << "Upper bound is: " << limit << "\r\n";
+      
     Step* solution = NULL;
     Item* initial = generateInitState(n, s);
     initial->getPole(f)->setFinal(true);
     Item* next = NULL;
     Stack* stack = new Stack();
     stack->push(initial);
-    
+    Item* item = NULL;
     while(!stack->isEmpty())
     {
-    //    while(counter < limit){
-            counter++;
-            Item* item = stack->head();
-            if(item == NULL) {
-                //stack is empty
-                break;
-            }
+			
+            item = stack->head();
             if(n == item->getPole(f)->getNoDiscs()){
-                // volat destrukci predchoziho reseni
+                if(solution != NULL) {
+					delete solution;
+					solution = NULL;
+				}
                 int disc;
                 int from;
                 int to;
-                while(NULL != item->getPrevious()){
+				limit = item->getRecursionLevel() - 1;
+				cout << "Found solution on " << limit+1 << " moves, lowering upper bound to " << limit << "\n";
+                while(NULL != item->getPreviousStep()){
                     disc = item->getPole(item->getStep())->getLastDisc()->getSize();
                     from = item->getActivePole();
                     to = item->getStep();
                     solution = new Step(disc, from, to, solution);
-                    item = item->getPrevious();
+					item = item->getPreviousStep();
                     
                 }
                 
-                limit = counter - 1;
+                
             }
+			if(!item->hasOption()){
+                delete stack->pop();
+                continue;
+            }
+			if(item->getRecursionLevel() >= limit) {
+				delete stack->pop();
+				continue;
+			}
             
             
                 
@@ -78,30 +94,23 @@ int main(int argc, char** argv) {
                 while(item->hasOption()){
                     int* step = item->popOption();
 					next = new Item(*item);
+					next->incrementRecursionLevel();
                     next->setPreviousStep(item);
                     next->doStep(step);
 					next->generateOptions();
                     stack->push(next);
                 }
-            
-			if(!item->hasOption()){
-                delete stack->pop();
-                counter--;
-                continue;
-            }
-			if(counter > limit) {
-				delete stack->pop();
-				counter--;
-				continue;
-			}
-        //}
     }
-    
+	cout << "\n\n-------------\n";
+    cout << "I have solution! \n";
     cout <<"Number of moves: " << (limit+1) << "\r\n";
-    while(solution->hasNext()){
-        cout <<"Disk: " << solution->getDisc() << " Z: " << solution->getFrom() << " Na: " << solution->getTo()<< "\r\n";
+    while(solution != NULL){
+        cout <<"Move disc " << solution->getDisc() << " from " << solution->getFrom() << " to " << solution->getTo()<< "\r\n";
         
         solution = solution->getNext();
     }
+	getchar();
     return 0;
 }
+
+
